757c8a39d11284f5432ca2fde8dfd875aea2473b
Nikolett Dékány <d.nikolett2@gmail.com>
2026-01-02 09:33:53 +0100

    fix: update Storyblok access token env variables

--- gc-project/src/lib/storyblok/api.ts
+++ gc-project/src/lib/storyblok/api.ts
@@ -4,14 +4,14 @@ import StoryblokClient from "storyblok-js-client";
 import components from "./components";
 
 storyblokInit({
-  accessToken: process.env.STORYBLOK_DELIVERY_API_PREVIEW_TOKEN,
+  accessToken: process.env.NEXT_STORYBLOK_DELIVERY_API_PREVIEW_TOKEN,
   use: [apiPlugin],
   apiOptions: { region: "eu" },
   components,
 });
 
 export default function getStoryblokApi() {
-  const accessToken = process.env.STORYBLOK_DELIVERY_API_PREVIEW_TOKEN;
+  const accessToken = process.env.NEXT_STORYBLOK_DELIVERY_API_PREVIEW_TOKEN;
 
   return new StoryblokClient({
     accessToken,
